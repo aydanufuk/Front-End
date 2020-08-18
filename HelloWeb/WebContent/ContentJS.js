d733558575168a20cba7cc04659a96d199f6e18a
UFUK AYDAN <69341397+aydanufuk@users.noreply.github.com>
2020-08-18 10:08:45 +0300

    Add files via upload

---
+++ HelloWeb/WebContent/ContentJS.js
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+
+function add(a,b){
+	return  a+b;
+}
+
+function sayHelloAgain(who){
+	document.write("hello :D " + who);
+}
+
+document.write("Hello world" + "<br/>");
+
+var myVariable = 5;
+
+var a = new Array(7);
+a[0] = "cat";
+a[1] = "dog";
+
+var x = 5;
+if(x > 3)
+	{
+	alert(x);
+	}
+
+for (var i = 0; i < 4; i++) {
+	document.write("ua");
+}
